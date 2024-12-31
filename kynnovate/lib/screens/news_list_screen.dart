7ff9aa6a9f9451fa8f6740cd29becee712c36fea
kavirajmetech <kavirajmetech@gmail.com>
2024-12-31 11:32:20 +0530

    hhhhh

--- kynnovate/lib/screens/news_list_screen.dart
+++ kynnovate/lib/screens/news_list_screen.dart
@@ -129,7 +129,7 @@ class _NewsListScreenState extends State<NewsListScreen> {
       //       onPressed: _refreshNews,
       //     ),
       //   ],
-      // ),
+      // ),   
       body: errorMessage.isNotEmpty
           ? _buildErrorWidget()
           : RefreshIndicator(
